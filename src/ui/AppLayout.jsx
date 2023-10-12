98dc9cef6ef3601f1d2666b47e63ccfbfd4f5552
makwaje <alloushi2010@gmail.com>
2023-10-12 20:53:06 +0200

    fix the favIcon and hide the scrollBar

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -7,6 +7,10 @@ const Main = styled.main`
   background-color: var(--color-grey-50);
   padding: 4rem 4.8rem 6.4rem;
   overflow: scroll;
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
 `;
 
 const StyledAppLayout = styled.div`

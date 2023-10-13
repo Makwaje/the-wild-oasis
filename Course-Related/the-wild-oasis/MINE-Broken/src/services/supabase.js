2ed0a6ad973bb670474b46dedd34e4fa87b65f2d
Mohamed Bakri <98839008+Makwaje@users.noreply.github.com>
2023-10-13 08:14:46 +0200

    Delete Course-Related directory

--- Course-Related/the-wild-oasis/MINE-Broken/src/services/supabase.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-export const supabaseUrl = "https://edgcrtyvwazjvrlaghth.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVkZ2NydHl2d2F6anZybGFnaHRoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTU1NjIxMDgsImV4cCI6MjAxMTEzODEwOH0.BjHEL2MGnEOX_b8srW6HzgT6Ko-84yHz0vfIEDoJyM0";
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-export default supabase;
